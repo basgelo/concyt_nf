2b3b883507c65e9c8d897dde8aeda31ab422f45f
Gelo <Gelo@GM-PC>
2017-01-30 13:21:14 +0100

    Revert "Changes on classes"
    This reverts commit 62fba4863a19b342afa08fc7c8e38c541b43f3c7.

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 include('classes/Persona.class.php');
 include('classes/ProspectoBecario.class.php');
-include('classes/Becas.class.php');
-include('classes/SystemUser.class.php');
-echo "Hello world!!!, this is running on Apache and PHP, learning PHP for the very first time, going well";
+echo "Hello world!!!, this is running on Apache and PHP 5.6, learning PHP for the very first time, going well";
 ?>
