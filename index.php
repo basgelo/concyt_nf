62fba4863a19b342afa08fc7c8e38c541b43f3c7
Gelo <Gelo@GM-PC>
2017-01-30 13:12:40 +0100

    Changes on classes
    New classes: Becas, SysteUser.
    Persona and Prospecto becario have been re-structured to manage in a
    better way all the properties in the objects.

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 include('classes/Persona.class.php');
 include('classes/ProspectoBecario.class.php');
-echo "Hello world!!!, this is running on Apache and PHP 5.6, learning PHP for the very first time, going well";
+include('classes/Becas.class.php');
+include('classes/SystemUser.class.php');
+echo "Hello world!!!, this is running on Apache and PHP, learning PHP for the very first time, going well";
 ?>
